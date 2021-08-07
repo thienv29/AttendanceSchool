9a72f342d53fc6c982dd37d550ab1d2a42110676
thienv29 <thienv29@gmail.com>
2021-08-07 17:54:28 +0700

    d

--- dffff/WebDiemDanh/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","","webdiemdanh");
-    mysqli_set_charset($conn,"utf8");
-?>
--- dffff/WebDiemDanh/test.php
+++
@@ -1,5 +0,0 @@
-<?php echo "100% <script>const percentCircle = document.querySelector('.course-percent .course-percent-circle:nth-child(2)')
-        const spanPercent = document.querySelector('.course-percent-text').innerHTML
-        percentInt = parseInt(spanPercent.split('%')[0])
-        percentCircle.style.strokeDasharray = '220'
-        percentCircle.style.strokeDashoffset = `calc(220 - (220*90)/100` </script>"?>
